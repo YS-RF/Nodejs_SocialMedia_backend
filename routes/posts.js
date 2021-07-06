a687e5455b81567493b9c048755f4d02bee66382
YS-RF <yanlin.song@sjsu.edu>
2021-07-06 10:03:02 -0700

    finished the first video

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 //create a post
 router.post("/", async (req, res) => {
@@ -70,15 +71,15 @@ router.get("/:id/", async (req, res) => {
 })
 //get timeline posts
 
-router.get("/timeline", async (req, res) => {
+router.get("/timeline/all", async (req, res) => {
 
   let postArray = [];
   try {
-    const currentUser = await User.findById(req.body.id);
+    const currentUser = await User.findById(req.body.userId);
     const userPosts = await Post.find({userId: currentUser._id});
     const friendPost = await Promise.all(
       currentUser.followings.map((friendId)=>{
-          Post.find({userId: friendId});
+          return Post.find({userId: friendId});
         })
       );
     res.json(userPosts.concat(...friendPost))
